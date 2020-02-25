d2ee27559d770e231bf7f857a85c4179426d1591
ktw361 <zhifanzhu@yeah.net>
2020-02-25 17:46:52 +0800

    remove *.d; Course finished!

--- assignments/PA4/cool-tree.d
+++
@@ -1,4 +0,0 @@
-cool-tree.o cool-tree.d : cool-tree.cc ../../include/PA4/tree.h \
-  ../../include/PA4/copyright.h ../../include/PA4/stringtab.h \
-  ../../include/PA4/list.h ../../include/PA4/cool-io.h \
-  cool-tree.handcode.h ../../include/PA4/cool.h cool-tree.h
